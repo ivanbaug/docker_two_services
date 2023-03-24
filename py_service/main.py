a74ec18f9d8994a547bff59918d57c5e7ba01036
ivanbaug <ivanbgp@gmail.com>
2023-03-23 21:57:04 -0500

    feat: Use deck-chores to automate running a python script

--- py_service/main.py
+++ py_service/main.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
+
 def run():
-    print('hey')
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time: ", current_time)
 
 if __name__ == "__main__":
     run()
