4e5934bd554c488dfc7cd567d7745152bd97737e
ivanbaug <ivanbgp@gmail.com>
2023-03-24 21:52:18 -0500

    feat: add volume and loggingto the py script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # docker_two_services
-Run a couple of python services from docker containers and crontab
+Originally the objective was to run a couple of python services/commands from docker containers and crontab.
+
+But crontab is tricky with docker and there was a solution that fits my needs and it's actively mantained (at least to the date of this commit).
+
+https://deck-chores.readthedocs.io/en/stable/usage.html#on-a-single-host
+
+
+
+
+## Volume to save logging data
+
+```bash
+docker volume create vol_pywebs
+```
--- py_service/main.py
+++ py_service/main.py
@@ -1,10 +1,17 @@
 from datetime import datetime
-
+import logging
+VOLUME_PATH = '/var/vol_pywebs'
+logging.basicConfig(
+    filename=f'{VOLUME_PATH}/app.log',
+    filemode='a',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+logging.warning('This will get logged to a file')
 
 def run():
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    print("Current Time: ", current_time)
+    # now = datetime.now()
+    # current_time = now.strftime("%H:%M:%S")
+    logging.warning("Logging test")
 
 if __name__ == "__main__":
     run()
